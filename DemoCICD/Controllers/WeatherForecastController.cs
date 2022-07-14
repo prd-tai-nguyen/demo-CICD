96dadbf04616df453144fe4271c8c16c77962d65
prd-tai-nguyen <91877469+prd-tai-nguyen@users.noreply.github.com>
2022-07-14 17:08:10 +0700

    Update WeatherForecastController.cs

--- DemoCICD/Controllers/WeatherForecastController.cs
+++ DemoCICD/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace DemoCICD.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "1", "2", "3", "4", "5", "6"
+            "1"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
